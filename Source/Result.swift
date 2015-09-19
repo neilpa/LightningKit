6de5cc459d426bff19e805ec019932a41189d72d
Neil Pankey <npankey@gmail.com>
2015-09-19 14:05:18 -0700

    Generalized `transact` method for Result
    Another helper for using Result with "resources" that have transaction
    semantics requiring specialized cleanup.

--- Source/Result.swift
+++ Source/Result.swift
@@ -16,6 +16,24 @@ internal extension Result {
     internal static func fsError(error: ErrorType) -> Result<T, ElephantError> {
         return .Failure(.FileSystem(error))
     }
+
+    /// Helper for consuming a "resource" with transactional semantics.
+    internal func transact<U>(acquire: () -> Result, consume: T -> Result<U, Error>, commit: T -> Error?, abort: T -> ()) -> Result<U, Error> {
+        return acquire().flatMap { resource in
+            return consume(resource).analysis(
+                ifSuccess: { value in
+                    if let error = commit(resource) {
+                        return .Failure(error)
+                    }
+                    return Result<U, Error>.Success(value)
+                },
+                ifFailure: { error in
+                    abort(resource)
+                    return .Failure(error)
+                }
+            )
+        }
+    }
 }
 
 internal func lmdbTry<A, B, C>(a: A, _ b: B, _ c: C, _ fn: (A, B, C, UnsafeMutablePointer<COpaquePointer>) -> Int32) -> Result<COpaquePointer, ElephantError> {
