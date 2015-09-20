2b7b6175758b49c4eda211fe1a70b37a955bfc3d
Neil Pankey <npankey@gmail.com>
2015-09-19 18:00:52 -0700

    Stub iOS demo

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  LKDemo
+//
+//  Created by Neil Pankey on 9/19/15.
+//  Copyright © 2015 Neil Pankey. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LKDemo
+//
+//  Created by Neil Pankey on 9/19/15.
+//  Copyright © 2015 Neil Pankey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
